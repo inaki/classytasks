5954628f8bc53b8c17af01f5328651ff086c37ba
inaki <iiaranzadi@gmail.com>
2014-04-28 16:44:34 -0700

    added TeacherMetricView and Controller, plus some modification to parentsView.html

---
+++ js/TeacherMetricView.js
@@ -0,0 +1,8 @@
+function TeacherMetricCtrl($scope) {
+ 
+  $scope.metrics = [
+    {name:'Miss Piggy', child: 'Piggy Boy', email: 'pigsinaction@gmail.com', phone: '(618) 784-9304',  points: 13, tasks: 5},
+    {name:'Mr. Gonzo', child: 'Gonzilla', email: 'gozorro@yahoo.com', phone: '(787) 394-9039', points: 33, tasks: 7}
+  ];
+
+ }
