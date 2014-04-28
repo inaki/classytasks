f73a35390e3924f61ec33f7d9ec3fbec0850d164
inaki <iiaranzadi@gmail.com>
2014-04-28 15:46:05 -0700

    List of needs coded in the view with angular and javascript code for controlling the view

---
+++ js/teacherView.js
@@ -0,0 +1,29 @@
+function TeacherCtrl($scope) {
+ 
+  $scope.needs = [
+    {name:'Pizza Party', need: 'Soda and Chips', date: '05/05/14-05/22/14', description: 'This is a party to reward the good work of students in the Math class', who: 'Imanol Aranzadi'},
+    {name:'Independence Day', need: 'Patriotic Hats', date: '07/04/14', description: 'This is a party to celebrate the independence of USA', who: 'Matthew Loveless'}
+  ];
+
+  // $scope.addNeed = function() {
+  //   $scope.todos.push({text:$scope.todoText, done:false});
+  //   $scope.todoText = '';
+  // };
+ 
+  // $scope.remaining = function() {
+  //   var count = 0;
+  //   angular.forEach($scope.todos, function(todo) {
+  //     count += todo.done ? 0 : 1;
+  //   });
+  //   return count;
+  // };
+ 
+  // $scope.archive = function() {
+  //   var oldTodos = $scope.todos;
+  //   $scope.todos = [];
+  //   angular.forEach(oldTodos, function(todo) {
+  //     if (!todo.done) $scope.todos.push(todo);
+  //   });
+  // };
+}
+
