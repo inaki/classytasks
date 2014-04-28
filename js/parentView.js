40e7e90d03d5709e6d7d806a7e827961b1e4d7ee
inaki <iiaranzadi@gmail.com>
2014-04-28 16:26:08 -0700

    added images for parents, and parentsView.html and teacherView.html

---
+++ js/parentView.js
@@ -0,0 +1,8 @@
+function ParentCtrl($scope) {
+ 
+  $scope.parents = [
+    {name:'Pizza Party', need: 'Soda and Chips', date: '05/05/14-05/22/14', description: 'This is a party to reward the good work of students in the Math class', vname: 'Mr. Gonzo', vimg: 'images/gonzo.png', points: 13},
+    {name:'Independence Day', need: 'Patriotic Hats', date: '07/04/14', description: 'This is a party to celebrate the independence of USA', vname: 'Miss Piggy', vimg: 'images/piggy.png', points: 33}
+  ];
+
+ }
--- js/teacherView.js
+++ js/teacherView.js
@@ -1,8 +1,8 @@
 function TeacherCtrl($scope) {
  
   $scope.needs = [
-    {name:'Pizza Party', need: 'Soda and Chips', date: '05/05/14-05/22/14', description: 'This is a party to reward the good work of students in the Math class', who: 'Imanol Aranzadi'},
-    {name:'Independence Day', need: 'Patriotic Hats', date: '07/04/14', description: 'This is a party to celebrate the independence of USA', who: 'Matthew Loveless'}
+    {name:'Pizza Party', need: 'Soda and Chips', date: '05/05/14-05/22/14', description: 'This is a party to reward the good work of students in the Math class', vname: 'Mr. Gonzo', vimg: 'images/gonzo.png'},
+    {name:'Independence Day', need: 'Patriotic Hats', date: '07/04/14', description: 'This is a party to celebrate the independence of USA', vname: 'Miss Piggy', vimg: 'images/piggy.png'}
   ];
 
   // $scope.addNeed = function() {
