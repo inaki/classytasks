4b845646f820bc301c7b58aaa3873755415c8685
inaki <iiaranzadi@gmail.com>
2014-04-29 14:12:59 -0700

    added the necessarry head links for make the angular bootstrap ui work, and copy/paste all the code of forms.js to partensView.js

--- js/parentView.js
+++ js/parentView.js
@@ -1,3 +1,5 @@
+angular.module('taskSignup', ['ui.bootstrap']);
+
 function ParentCtrl($scope) {
  
   $scope.parents = [
@@ -11,6 +13,85 @@ function ParentCtrl($scope) {
  }
 
  var ModalDemoCtrl = function ($scope, $modal, $log) {
+
+    $scope.data = {
+    };
+
+    $scope.signup = function () {
+
+        $modal.open({
+            templateUrl: 'signup.html',
+            backdrop: true,
+            windowClass: 'modal',
+            controller: function ($scope, $modalInstance, $log, data) {
+                $scope.data = data;
+                $scope.submit = function () {
+                    $log.log('Submitting info.');
+                    $log.log(JSON.stringify(data));
+                    $modalInstance.dismiss('cancel');
+                }
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+                data: function () {
+                    return $scope.data;
+                }
+            }
+        });
+    };
+    $scope.addtask = function () {
+
+        $modal.open({
+            templateUrl: 'addtask.html',
+            backdrop: true,
+            windowClass: 'modal',
+            controller: function ($scope, $modalInstance, $log, data) {
+                $scope.data = data;
+                $scope.submit = function () {
+                    $log.log('Submitting info.');
+                    $log.log(JSON.stringify(data));
+                    $modalInstance.dismiss('cancel');
+                }
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+                data: function () {
+                    return $scope.data;
+                }
+            }
+        });
+    };
+    $scope.contact = function () {
+
+        $modal.open({
+            templateUrl: 'contact.html',
+            backdrop: true,
+            windowClass: 'modal',
+            controller: function ($scope, $modalInstance, $log, data) {
+                $scope.data = data;
+                $scope.submit = function () {
+                    $log.log('Submitting info.');
+                    $log.log(JSON.stringify(data));
+                    $modalInstance.dismiss('cancel');
+                }
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+                data: function () {
+                    return $scope.data;
+                }
+            }
+        });
+    };        
+};
+
+var ModalDemoCtrl = function ($scope, $modal, $log) {
 
     $scope.data = {
     };
