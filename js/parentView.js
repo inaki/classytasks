77a73effdf549cd627557253841f90902a831c53
inaki <iiaranzadi@gmail.com>
2014-04-29 04:16:12 -0700

    add navbar to every view, and some css to adapt the navbar to how is in the index, which i recommended to be change using twitter bootstrap

--- js/parentView.js
+++ js/parentView.js
@@ -9,3 +9,82 @@ function ParentCtrl($scope) {
   ];
 
  }
+
+ var ModalDemoCtrl = function ($scope, $modal, $log) {
+
+    $scope.data = {
+    };
+
+    $scope.signup = function () {
+
+        $modal.open({
+            templateUrl: 'signup.html',
+            backdrop: true,
+            windowClass: 'modal',
+            controller: function ($scope, $modalInstance, $log, data) {
+                $scope.data = data;
+                $scope.submit = function () {
+                    $log.log('Submitting info.');
+                    $log.log(JSON.stringify(data));
+                    $modalInstance.dismiss('cancel');
+                }
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+                data: function () {
+                    return $scope.data;
+                }
+            }
+        });
+    };
+    $scope.addtask = function () {
+
+        $modal.open({
+            templateUrl: 'addtask.html',
+            backdrop: true,
+            windowClass: 'modal',
+            controller: function ($scope, $modalInstance, $log, data) {
+                $scope.data = data;
+                $scope.submit = function () {
+                    $log.log('Submitting info.');
+                    $log.log(JSON.stringify(data));
+                    $modalInstance.dismiss('cancel');
+                }
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+                data: function () {
+                    return $scope.data;
+                }
+            }
+        });
+    };
+    $scope.contact = function () {
+
+        $modal.open({
+            templateUrl: 'contact.html',
+            backdrop: true,
+            windowClass: 'modal',
+            controller: function ($scope, $modalInstance, $log, data) {
+                $scope.data = data;
+                $scope.submit = function () {
+                    $log.log('Submitting info.');
+                    $log.log(JSON.stringify(data));
+                    $modalInstance.dismiss('cancel');
+                }
+                $scope.cancel = function () {
+                    $modalInstance.dismiss('cancel');
+                };
+            },
+            resolve: {
+                data: function () {
+                    return $scope.data;
+                }
+            }
+        });
+    };        
+};
